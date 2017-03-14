8bbaaa0be03afd209535f846e0b7acc5d1715339
1141384041_qq.com_xinglong <1141384041@qq.com>
2017-03-14 21:07:45 +0800

    提交主页

---
+++ src/main/java/com/demo/common/DemoConfig.java
@@ -0,0 +1,128 @@
+package com.demo.common;
+
+import com.demo.common.model._MappingKit;
+import com.demo.modules.blog.BlogController;
+import com.demo.modules.sys.IndexController;
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.core.JFinal;
+import com.jfinal.kit.PropKit;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.template.Engine;
+
+/**
+ * 本 demo 仅表达最为粗浅的 jfinal 用法，更为有价值的实用的企业级用法
+ * 详见 JFinal 俱乐部: http://jfinal.com/club
+ * 
+ * API引导式配置
+ */
+public class DemoConfig extends JFinalConfig {
+	
+	/**
+	 * 运行此 main 方法可以启动项目，此main方法可以放置在任意的Class类定义中，不一定要放于此
+	 * 
+	 * 使用本方法启动过第一次以后，会在开发工具的 debug、run config 中自动生成
+	 * 一条启动配置，可对该自动生成的配置再添加额外的配置项，例如 VM argument 可配置为：
+	 * -XX:PermSize=64M -XX:MaxPermSize=256M
+	 */
+	public static void main(String[] args) {
+		/**
+		 * 特别注意：Eclipse 之下建议的启动方式
+		 */
+		JFinal.start("src/main/webapp", 80, "/", 5);
+		
+		/**
+		 * 特别注意：IDEA 之下建议的启动方式，仅比 eclipse 之下少了最后一个参数
+		 */
+		// JFinal.start("src/main/webapp", 80, "/");
+	}
+	
+	/**
+	 * 配置常量
+	 */
+	public void configConstant(Constants me) {
+		// 加载少量必要配置，随后可用PropKit.get(...)获取值
+		PropKit.use("a_little_config.txt");
+		me.setDevMode(PropKit.getBoolean("devMode", false));
+		
+		// PropKit 的大致使用方式
+		/*Prop p = PropKit.use("db_config.txt");
+		DruidPlugin dp = new DruidPlugin(p.get("jdbcUrl"), p.get("user")…);
+		me.add(dp);*/
+	}
+	
+	/**
+	 * 配置路由(url,跳转对应controller)
+	 */
+	public void configRoute(Routes me) {
+		me.add("/", IndexController.class, "/modules/sys");	// 第三个参数为该Controller的视图存放路径
+		me.add("/blog", BlogController.class);			// 第三个参数省略时默认与第一个参数值相同，在此即为 "/blog"
+	}
+	
+	/**
+	 * 前端页面模版 定义
+	 */
+	public void configEngine(Engine me) {
+		me.addSharedFunction("/WEB-INF/layout/_layout.html");
+		me.addSharedFunction("/WEB-INF/layout/_paginate.html");
+		me.addSharedFunction("/WEB-INF/layout/_top.html");
+	}
+	
+	/**
+	 * 配置插件
+	 */
+	public void configPlugin(Plugins me) {
+		// 配置C3p0数据库连接池插件
+		DruidPlugin druidPlugin = new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
+		me.add(druidPlugin);
+		
+		// 配置ActiveRecord插件
+		ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
+		// 所有映射在 MappingKit 中自动化搞定
+		_MappingKit.mapping(arp);
+		me.add(arp);
+	}
+	
+	public static DruidPlugin createDruidPlugin() {
+		return new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
+	}
+	
+	/**
+	 * 配置全局拦截器
+	 */
+	public void configInterceptor(Interceptors me) {
+		//写法类似于  me.add(new AuthInterceptor());
+	}
+	
+	/**
+	 * 配置处理器(可以配置全局变量什么的)
+	 */
+	public void configHandler(Handlers me) {
+		
+	}
+
+	/**
+	 * jFinal启动后运行
+	 */
+	@Override
+	public void afterJFinalStart() {
+		// TODO Auto-generated method stub
+		super.afterJFinalStart();
+	}
+
+	/**
+	 * jFinal 启动前执行
+	 */
+	@Override
+	public void beforeJFinalStop() {
+		// TODO Auto-generated method stub
+		super.beforeJFinalStop();
+	}
+	
+	
+}
